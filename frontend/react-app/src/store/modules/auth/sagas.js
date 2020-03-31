df04bc125a96715ef21ddd84b0bb07c300b694a0
Guilherme de Favere <guilherme.favere@gmail.com>
2020-03-30 21:09:24 -0300

    Config loading during login

--- frontend/react-app/src/store/modules/auth/sagas.js
+++ frontend/react-app/src/store/modules/auth/sagas.js
@@ -1,22 +1,26 @@
 import { all, takeLatest, call, put } from "redux-saga/effects";
 
-import { signInSuccess } from "./actions";
+import { signInSuccess, signFailure } from "./actions";
 import api from "~/services/api";
 import history from "~/services/history";
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
-  const res = yield call(api.post, "sessions", { email, password });
-  const { token, user } = res.data;
+  try {
+    const { email, password } = payload;
+    const res = yield call(api.post, "sessions", { email, password });
+    const { token, user } = res.data;
 
-  if (!user.provider) {
-    console.tron.error("Usuário não é prestador");
-    return;
+    if (!user.provider) {
+      console.tron.error("Usuário não é prestador");
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push("/dashboard");
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push("/dashboard");
 }
 
 export default all([takeLatest("@auth/SIGN_IN_REQUEST", signIn)]);
