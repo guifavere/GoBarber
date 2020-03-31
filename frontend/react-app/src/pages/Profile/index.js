a98138e52f3a3c7e05a75ab6641706bd59b66b4d
Guilherme de Favere <guilherme.favere@gmail.com>
2020-03-31 20:50:21 -0300

    Create profile page

--- frontend/react-app/src/pages/Profile/index.js
+++ frontend/react-app/src/pages/Profile/index.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { Form, Input } from "@rocketseat/unform";
+import { useSelector } from "react-redux";
+
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <p>Profile</p>;
+  const profile = useSelector(state => state.user.profile);
+
+  const handleSubmit = data => {};
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
