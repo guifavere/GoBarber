e47b0c76f1ca3fdb6b3d2c5de0b23b62e7e61adc
Guilherme de Favere <guilherme.favere@gmail.com>
2020-04-01 13:14:41 -0300

    Create style for the dashboard page

--- frontend/react-app/src/pages/Dashboard/index.js
+++ frontend/react-app/src/pages/Dashboard/index.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 import api from "~/services/api";
 
-export default function Dashboard() {
-  api.get("appointments");
+import { Container, Time } from "./styles";
 
-  return <p>Dashboard</p>;
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Fulano de tal</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Fulano de tal</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Fulano de tal</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
