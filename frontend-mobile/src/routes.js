4ca6729d1edf7e8b7b3f0e52b97f0468cd47e1d3
Guilherme de Favere <guilherme.favere@gmail.com>
2020-04-04 17:10:37 -0300

    Config routes

---
+++ frontend-mobile/src/routes.js
@@ -0,0 +1,20 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+export default function Routes() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={SignIn} name="SignIn" />
+        <Stack.Screen component={SignUp} name="SignUp" />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
