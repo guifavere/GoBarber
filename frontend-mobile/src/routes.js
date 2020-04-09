6c096b3af0b4783d19c3e924b82ea3399425e1c4
Guilherme de Favere <guilherme.favere@gmail.com>
2020-04-08 21:45:36 -0300

    Config authenticated routes

--- frontend-mobile/src/routes.js
+++ frontend-mobile/src/routes.js
@@ -1,19 +1,37 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import Dashboard from './pages/Dashboard';
+
 export default function Routes() {
+  const signed = useSelector((state) => state.auth.signed);
   const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen component={SignIn} name="SignIn" />
-        <Stack.Screen component={SignUp} name="SignUp" />
+      <Stack.Navigator>
+        {!signed ? (
+          <>
+            <Stack.Screen
+              component={SignIn}
+              name="SignIn"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              component={SignUp}
+              name="SignUp"
+              options={{ headerShown: false }}
+            />
+          </>
+        ) : (
+          <Stack.Screen component={Dashboard} name="Dashboard" />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
