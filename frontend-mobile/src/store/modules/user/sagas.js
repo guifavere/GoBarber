cfafdbc351a3b8c7ce3cca52a5fbb3f21c920ba1
Guilherme de Favere <guilherme.favere@gmail.com>
2020-04-11 13:38:08 -0300

    Create profile page

--- frontend-mobile/src/store/modules/user/sagas.js
+++ frontend-mobile/src/store/modules/user/sagas.js
@@ -6,21 +6,20 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.old_password ? rest : {}),
     };
 
     const res = yield call(api.put, 'users', profile);
 
-    Alert('Sucesso', 'Perfil atualizado com sucesso');
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso');
 
     yield put(updateProfileSuccess(res.data));
   } catch (err) {
-    Alert(
+    Alert.alert(
       'Falha na atualização',
       'Houve um erro na atualização do perfil, verifique seus dados'
     );
