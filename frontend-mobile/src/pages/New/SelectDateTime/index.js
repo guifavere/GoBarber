fba486b672f3fd3d15a0ea5215ed49ab8df2bf26
Guilherme de Favere <guilherme.favere@gmail.com>
2020-04-14 20:59:06 -0300

    Create the function that select hour, and redirect user to confirm page

--- frontend-mobile/src/pages/New/SelectDateTime/index.js
+++ frontend-mobile/src/pages/New/SelectDateTime/index.js
@@ -1,17 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-import { Container } from './styles';
+import api from '~/services/api';
 
-export default function SelectDateTime() {
+import { Container, HourList, Hour, Title } from './styles';
+
+export default function SelectDateTime({ route, navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const { provider } = route.params;
+
+  const handleSelectHour = (time) =>
+    navigation.navigate('Confirm', { provider, time });
+
+  useEffect(() => {
+    const loadAvailable = async () => {
+      const res = await api.get(`providers/${provider.id}/available`, {
+        params: { date: date.getTime() },
+      });
+
+      setHours(res.data.available);
+    };
+
+    loadAvailable();
+  }, [date, provider.id]);
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+        <HourList
+          data={hours}
+          keyExtractor={(item) => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => handleSelectHour(item.value)}
+              enabled={item.available}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
